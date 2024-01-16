cd93840fb815b26436d57a184a8e629eb54ef745
Sheikh Saad <sheikhsaadsd@gmail.com>
2024-01-16 16:23:11 +0100

    VCSConsole

---
+++ database/migrations/2024_01_16_135307_create_v_c_s_consoles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('v_c_s_consoles', function (Blueprint $table) {
+            $table->id();
+            $table->string('token_id');
+            $table->string('instanceId');
+            $table->string('publicDnsName');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('v_c_s_consoles');
+    }
+};
